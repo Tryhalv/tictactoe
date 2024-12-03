9c904582a285b54217cb3152df61a6909e7bedfa
Trym Halvorsen <1trymhalvorsen@gmail.com>
2024-12-03 21:42:59 +0100

    first

---
+++ Board.js
@@ -0,0 +1,12 @@
+const tttb = function (brett) {
+  `
+╔═══╦═══╦═══╗
+║ ${brett[0][0]}  ║${brett[0][1]}   ║${brett[0][2]}   ║
+╠═══╬═══╬═══╣
+║ ${brett[1][0]}  ║${brett[1][1]}   ║${brett[1][2]}   ║
+╠═══╬═══╬═══╣
+║ ${brett[2][0]}  ║${brett[2][1]}   ║${brett[2][2]}   ║
+╚═══╩═══╩═══╝`;
+};
+
+export default tttb;
